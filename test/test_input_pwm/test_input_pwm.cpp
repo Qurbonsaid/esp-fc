b07dbc67d4d8b30ec1914d16bcbe452d4ae140a6
Qurbonsaid <Kurbonsaid_Rayimov@student.itpu.uz>
2025-07-22 13:14:29 +0000

    fix: avoid hardware dependency while testing

--- test/test_input_pwm/test_input_pwm.cpp
+++ test/test_input_pwm/test_input_pwm.cpp
@@ -1,7 +1,11 @@
 #include <unity.h>
+#include <ArduinoFake.h>
 #include "Device/InputPWM.h"
+#include "Device/InputDevice.h"
 
+using namespace Espfc;
 using namespace Espfc::Device;
+using namespace fakeit;
 
 void setUp() {
     // Setup before each test
@@ -13,6 +17,15 @@ void tearDown() {
 
 void test_pwm_input_initialization() {
     InputPWM pwm;
+    
+    // Mock Arduino functions
+    When(Method(ArduinoFake(), micros)).Return(0);
+    When(Method(ArduinoFake(), pinMode)).Return();
+    When(Method(ArduinoFake(), digitalPinToInterrupt)).Return(0);
+    When(Method(ArduinoFake(), attachInterrupt)).Return();
+    
+    // Initialize with dummy pins (they won't actually be used in test)
+    pwm.begin(2, 3, 4, 5);
     
     // Test that the device initializes correctly
     TEST_ASSERT_EQUAL(4, pwm.getChannelCount());
@@ -28,6 +41,15 @@ void test_pwm_input_initialization() {
 void test_pwm_input_get_channels() {
     InputPWM pwm;
     uint16_t channels[8];
+    
+    // Mock Arduino functions
+    When(Method(ArduinoFake(), micros)).Return(0);
+    When(Method(ArduinoFake(), pinMode)).Return();
+    When(Method(ArduinoFake(), digitalPinToInterrupt)).Return(0);
+    When(Method(ArduinoFake(), attachInterrupt)).Return();
+    
+    // Initialize with dummy pins
+    pwm.begin(2, 3, 4, 5);
     
     // Test getting channels into array
     pwm.get(channels, 8);
@@ -46,8 +68,39 @@ void test_pwm_input_get_channels() {
 void test_pwm_input_invalid_channel() {
     InputPWM pwm;
     
+    // Mock Arduino functions
+    When(Method(ArduinoFake(), micros)).Return(0);
+    When(Method(ArduinoFake(), pinMode)).Return();
+    When(Method(ArduinoFake(), digitalPinToInterrupt)).Return(0);
+    When(Method(ArduinoFake(), attachInterrupt)).Return();
+    
+    // Initialize with dummy pins
+    pwm.begin(2, 3, 4, 5);
+    
     // Test invalid channel returns safe default
     TEST_ASSERT_EQUAL(1500, pwm.get(10));
+}
+
+void test_pwm_input_update_status() {
+    InputPWM pwm;
+    
+    // Mock Arduino functions
+    When(Method(ArduinoFake(), micros)).Return(1000);
+    When(Method(ArduinoFake(), pinMode)).Return();
+    When(Method(ArduinoFake(), digitalPinToInterrupt)).Return(0);
+    When(Method(ArduinoFake(), attachInterrupt)).Return();
+    
+    // Initialize with dummy pins
+    pwm.begin(2, 3, 4, 5);
+    
+    // Test initial status
+    InputStatus status = pwm.update();
+    TEST_ASSERT_EQUAL(INPUT_IDLE, status);
+    
+    // Test timeout condition - simulate time passing without signal
+    When(Method(ArduinoFake(), micros)).Return(60000); // 60ms later
+    status = pwm.update();
+    TEST_ASSERT_EQUAL(INPUT_LOST, status);
 }
 
 int main() {
@@ -56,6 +109,7 @@ int main() {
     RUN_TEST(test_pwm_input_initialization);
     RUN_TEST(test_pwm_input_get_channels);
     RUN_TEST(test_pwm_input_invalid_channel);
+    RUN_TEST(test_pwm_input_update_status);
     
     return UNITY_END();
 }
